69a0961538244847d71dcefcca4bbbe59947ee4f
seono9y <144989211+seono9y@users.noreply.github.com>
2023-12-03 18:31:12 +0900

    1-2

--- exercise_13/exercise_13/main.c
+++ exercise_13/exercise_13/main.c
@@ -7,17 +7,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 struct student {
     int ID;
-    char name[100];
-    float score;
-};
+        char name[100];
+        float score;
+        };
 
 int main (void) {
-
-struct student s1 = {132, "seoyeon chu", 4.3};
-
-return 0;
+    struct student s1 = {2313800, "seoyeon", 4.3};
+    s1.ID = 2313;
+    s1.name[0] = 's';
+    s1.score = 4.20000;
+    printf("ID : %d\n", s1.ID);
+    printf("name : %s\n", s1.name);
+    printf("score : %f\n", s1.score);
+    
+    strcpy(s1.name, "JuYeop");
+    printf("name2: %s\n",s1.name);
 }
