a258b0ad944f1eb4850a170ad54870f57f1e0562
seono9y <144989211+seono9y@users.noreply.github.com>
2023-12-03 18:16:03 +0900

    1-1

---
+++ exercise_13/exercise_13/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  exercise_13
+//
+//  Created by 추서연 on 2023/12/03.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct student {
+    int ID;
+    char name[100];
+    float score;
+};
+
+int main (void) {
+
+struct student s1 = {132, "seoyeon chu", 4.3};
+
+return 0;
+}
